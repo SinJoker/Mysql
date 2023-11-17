4b02bffed0fbe4d51e465c841c98d54c1cef9cbb
SinJoker <51954088+SinJoker@users.noreply.github.com>
2023-11-17 17:05:01 +0000

    new file:   Basic_Query_Library.sql

---
+++ Basic_Query_Library.sql
@@ -0,0 +1,48 @@
+# 显示所有的数据库
+SHOW DATABASES;
+
+# 显示当前数据库的编码
+SHOW VARIABLES LIKE 'character%';
+
+# 新建库数据（如果不存在）
+CREATE DATABASE IF NOT EXISTS test;
+
+# 删除库数据（如果存在）
+DROP DATABASE IF EXISTS test;
+
+# 显示当前数据库
+SELECT DATABASE();
+
+# 切换数据库
+USE test;
+
+# 显示当前数据库的所有表
+SHOW TABLES;
+
+# 显示当前数据库的所有表结构
+SHOW CREATE TABLE test;
+
+# 新建表
+CREATE TABLE IF NOT EXISTS test(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20)
+);
+
+# 删除表
+DROP TABLE IF EXISTS test;
+
+# 显示表结构
+DESC test;
+
+# 插入数据
+INSERT INTO test(name) VALUES('张三');
+INSERT INTO test(name) VALUES('李四');
+
+#修改数据
+UPDATE test SET name='王五' WHERE id=1;
+
+# 删除数据
+DELETE FROM test WHERE id=1;
+
+# 查询数据
+SELECT * FROM test;

c8d38f93776f71d307729083e4208a65739adab5
SunJunBo_Company <15755526217@163.com>
2023-11-19 22:20:56 +0800

    modified:   formula_DataBase/1 build database and table.sql

--- formula_DataBase/1 build database and table.sql
+++ formula_DataBase/1 build database and table.sql
@@ -10,6 +10,16 @@ CREATE TABLE IF NOT EXISTS formula_category (
  category_name VARCHAR(255) UNIQUE NOT NULL
 );
 
+# 创建物理量参数表 formula_param
+CREATE TABLE IF NOT EXISTS formula_param (
+ param_id INT AUTO_INCREMENT PRIMARY KEY,
+ param_name VARCHAR(255) NOT NULL,
+ description TEXT,
+ # 有些参数没有固定数值，一般可以为空
+ param_value DOUBLE,
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+);
 
 # 创建主表 formula_main
 CREATE TABLE IF NOT EXISTS formula_main (
@@ -28,26 +38,16 @@ CREATE TABLE IF NOT EXISTS formula_category_relation (
  formula_id INT NOT NULL,
  category_id INT NOT NULL,
  PRIMARY KEY (formula_id, category_id),
- FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id)
-);
-
-# 创建公式参数表 formula_param
-CREATE TABLE IF NOT EXISTS formula_param (
- formula_param_id INT AUTO_INCREMENT PRIMARY KEY,
- formula_id INT NOT NULL,
- param_name VARCHAR(255) NOT NULL,
- description TEXT,
- # 有些参数没有固定数值，一般可以为空
- param_value DOUBLE,
- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id)
+ FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id),
+ FOREIGN KEY (category_id) REFERENCES formula_category(category_id)
 );
 
 CREATE TABLE IF NOT EXISTS formula_main_relation (
  formula_main_relation_id INT AUTO_INCREMENT PRIMARY KEY,
- formula_id INT ,
- formula_param_id INT ,
+ formula_id INT,
+ formula_param_id INT,
+ PRIMARY KEY (formula_id, formula_param_id),
  FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id),
- FOREIGN KEY (formula_param_id) REFERENCES formula_param(formula_param_id)
+ FOREIGN KEY (formula_param_id) REFERENCES formula_param(param_id)
 );
+

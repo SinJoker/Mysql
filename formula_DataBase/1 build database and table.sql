69e74f18b582cb74006150f740fe502b86ab8579
SinJoker <51954088+SinJoker@users.noreply.github.com>
2023-11-19 15:46:08 +0000

    modified:   formula_DataBase/1 build database and table.sql 	modified:   formula_DataBase/2 insert record to database.sql     进展：         初步建立功能完整的公式数据库。     问题：         但数据的更新后关系表的处理应该怎么办？

--- formula_DataBase/1 build database and table.sql
+++ formula_DataBase/1 build database and table.sql
@@ -10,10 +10,23 @@ CREATE TABLE IF NOT EXISTS formula_category (
  category_name VARCHAR(255) UNIQUE
 );
 
+# 创建公式参数表 formula_param
+CREATE TABLE IF NOT EXISTS formula_param (
+ param_id INT AUTO_INCREMENT PRIMARY KEY,
+ param_name VARCHAR(255) NOT NULL,
+ # 有些参数没有固定数值，一般可以为空
+ param_description TEXT,
+ # 有时候不一定是数值
+ param_value VARCHAR(255),
+ param_unit VARCHAR(255),
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 # 创建主表 formula_main
 CREATE TABLE IF NOT EXISTS formula_main (
- id INT AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(255) NOT NULL UNIQUE,
+ formula_id INT AUTO_INCREMENT PRIMARY KEY,
+ formula_name VARCHAR(255) NOT NULL UNIQUE,
  format_latex VARCHAR(255) NOT NULL UNIQUE,
  format_excel VARCHAR(255),
  description TEXT,
@@ -22,32 +35,21 @@ CREATE TABLE IF NOT EXISTS formula_main (
  updated_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-# 创建公式-类别和公式-主表之间的关联表formula_category_relation和formula_main_relation
+# 创建公式-类别和公式-主表之间的关联表formula_category_relation和formula_param_relation
 CREATE TABLE IF NOT EXISTS formula_category_relation (
  formula_id INT NOT NULL,
  formula_category_id INT NOT NULL,
  PRIMARY KEY (formula_id, formula_category_id),
  FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id),
- FOREIGN KEY (formula_category_id) REFERENCES formula_category(formula_category_id)
+ FOREIGN KEY (formula_category_id) REFERENCES formula_category(category_id)
 );
 
-# 创建公式参数表 formula_param
-CREATE TABLE IF NOT EXISTS formula_param (
- formula_param_id INT AUTO_INCREMENT PRIMARY KEY,
- formula_id INT NOT NULL,
- param_name VARCHAR(255) NOT NULL,
- # 有些参数没有固定数值，一般可以为空
- param_value DOUBLE ,
- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id)
-);
 
-CREATE TABLE IF NOT EXISTS formula_main_relation (
- formula_main_relation_id INT AUTO_INCREMENT PRIMARY KEY,
- formula_id INT ,
+CREATE TABLE IF NOT EXISTS formula_param_relation (
+ formula_id INT,
  formula_param_id INT ,
+ PRIMARY KEY (formula_id, formula_param_id),
  FOREIGN KEY (formula_id) REFERENCES formula_main(formula_id),
- FOREIGN KEY (formula_param_id) REFERENCES formula_param(formula_param_id)
+ FOREIGN KEY (formula_param_id) REFERENCES formula_param(param_id)
 );
 
--- formula_DataBase/2 insert record to database.sql
+++ formula_DataBase/2 insert record to database.sql
@@ -1,22 +1,43 @@
 # 插入常用的类目
-INSERT INTO formula_category (category_name)
-    VALUES ('会计学'),('流体力学'),('传热学'),('工程计算'),('其他');
+INSERT INTO formula_category 
+    (category_name)
+VALUES 
+    ('会计学'),
+    ('流体力学'),
+    ('传热学'),
+    ('工程计算'),
+    ('其他');
 
+INSERT INTO formula_param 
+    (param_name,
+    param_description,
+    param_value,
+    param_unit)
+VALUES
+    ('v','velocity,速度','','m/s'),
+    ('Q_{V}','Volume Flux,体积流量','','m^3/s'),
+    ('S','Cross Section Area,横截面积','','m^2'),
+    ('d','Diameter,直径','','m'),
+    ('pi','圆周率','3.14159265359','')
+;
 
 # 事务——插入一条数据
 START TRANSACTION;
 
     -- 向formula_main表中插入一条数据
-    INSERT INTO formula_main (formula_name, formula_latex, formula_excel, description, example_usage)
-    VALUES ('加法', 'a + b', '=', '这是一条加法公式的示例', '1 + 2 = 3');
+    INSERT INTO formula_main (formula_name, format_latex, format_excel, description, example_usage)
+    VALUES ('根据流量与管径计算流速', 'v= \\frac{Q_{V}}{S} = \\frac{Q_{V}}{\\frac{1}{4} \\pi d^{2}}', '=(Volume_FLux)/(3600/(0.25*pi()*(Diameter)^2)/1000000)', '根据体积流量、管道直径计算管内流速。', '无');
 
-    -- 向formula_category表中插入一条数据
-    INSERT INTO formula_category (formula_category_name)
-    VALUES ('数学');
-
-    -- 向formula_param表中插入一条数据
-    INSERT INTO formula_param (param_name, param_value)
-    VALUES ('a', 10), ('b', 5);
+    INSERT INTO formula_category_relation (formula_id,formula_category_id)
+    VALUES
+     (1, 4);
+    INSERT INTO formula_param_relation (formula_id,formula_param_id)
+    VALUES 
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5);
 
 -- 提交事务
 COMMIT;

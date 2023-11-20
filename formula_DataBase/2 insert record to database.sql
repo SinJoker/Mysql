d40cdfa7a721337d92392fa7c2c91163e37918c1
SunJunBo_Company <15755526217@163.com>
2023-11-20 16:56:45 +0800

    modified:   formula_DataBase/2 insert record to database.sql

--- formula_DataBase/2 insert record to database.sql
+++ formula_DataBase/2 insert record to database.sql
@@ -33,22 +33,26 @@ START TRANSACTION;
         ('传热学');
 
     # 向formula_main表中插入一条数据
-    INSERT INTO formula_main (formula_name, format_latex, format_excel, description, example_usage)
-    VALUES ('根据流量与管径计算流速', 'v= \\frac{Q_{V}}{S} = \\frac{Q_{V}}{\\frac{1}{4} \\pi d^{2}}', '=(Volume_FLux)/(3600/(0.25*pi()*(Diameter)^2)/1000000)', '根据体积流量、管道直径计算管内流速。', '无');
+    INSERT INTO formula_main 
+        (formula_name, format_latex, format_excel, description, example_usage)
+    VALUES 
+        ('根据流量与管径计算流速', 'v= \\frac{Q_{V}}{S} = \\frac{Q_{V}}{\\frac{1}{4} \\pi d^{2}}', '=(Volume_FLux)/(3600/(0.25*pi()*(Diameter)^2)/1000000)', '根据体积流量、管道直径计算管内流速。', '无');
 
     # 指定其类目
-    INSERT INTO formula_category_relation (formula_id,formula_category_id)
+    INSERT INTO formula_category_relation 
+        (formula_id,formula_category_id)
     VALUES
-     (1, 4);
+        (1, 4);
 
     # 指定其用到的参数
-    INSERT INTO formula_param_relation (formula_id,formula_param_id)
+    INSERT INTO formula_param_relation 
+        (formula_id,formula_param_id)
     VALUES 
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (1, 5);
+        (1, 1),
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (1, 5);
 
 -- 提交事务
 COMMIT;
